331a93a78f51332021b1bc05f0ddd08bcbc2470a
StuberSimon <stuber.simon@proton.me>
2026-04-06 10:58:18 +0200

    refactor: clean up code formatting

--- scripts_ba/calc_discharge_change.py
+++ scripts_ba/calc_discharge_change.py
@@ -1,7 +1,6 @@
-import xarray as xr
-import pandas as pd
 import matplotlib.pyplot as plt
 import numpy as np
+import xarray as xr
 
 ds = xr.open_dataset("data/data_version-4.0_consolidated.nc")
 
@@ -14,19 +13,21 @@ sel = ds.sel(
 # ── Plot 1: xarray pcolormesh for spatial snapshot (one day) ────────────────────
 snap = sel.dis24.sel(valid_time="2013-06-15")  # One snapshot
 snap.plot(figsize=(10, 6), cmap="YlGnBu")
-plt.plot(6.975000000006665,50.925000000002214, 'r.', markersize=10)
+plt.plot(6.975000000006665, 50.925000000002214, "r.", markersize=10)
 plt.title("River discharge central europe — 2013-06-15")
 plt.savefig("scripts_ba/results/discharge_snapshot_central_europe_1.png")
 
 # ── Plot 2: xarray line plot for time series at a single point (cologne) ────────────────
-point = sel.dis24.sel(latitude=50.925000000002214, longitude=6.975000000006665, method="nearest")
+point = sel.dis24.sel(
+    latitude=50.925000000002214, longitude=6.975000000006665, method="nearest"
+)
 point.plot(figsize=(12, 4))
 plt.title("Discharge time series in Rhein (50.925°N, 6.975°E)")
 plt.ylabel("dis24 (m³/s)")
 plt.savefig("scripts_ba/results/discharge_rhein_2013_1.png")
 
 
-cologne = point.to_dataframe().drop(columns=['surface','longitude','latitude'])
+cologne = point.to_dataframe().drop(columns=["surface", "longitude", "latitude"])
 
 cologne_shifted = cologne.shift(1)
 diff = cologne.iloc[1:-1] - cologne_shifted.iloc[1:-1]
@@ -36,9 +37,9 @@ diff_max = diff.abs().max().values[0]
 diff_max_rel = diff_max / cologne.abs().mean().values[0]
 diff_min = diff.abs().min().values[0]
 diff_min_rel = diff_min / cologne.abs().mean().values[0]
-print(f'- Average absolute change = {np.round(diff_avg*1, 2)} m³/s')
-print(f'- Average relative change = {np.round(diff_avg_rel*100, 2)}%')
-print(f'- Maximum absolute change = {np.round(diff_max*1, 2)} m³/s')
-print(f'- Maximum relative change = {np.round(diff_max_rel*100, 2)}%')
-print(f'- Minimum absolute change = {np.round(diff_min*1, 2)} m³/s')
-print(f'- Minimum relative change = {np.round(diff_min_rel*100, 3)}%')
+print(f"- Average absolute change = {np.round(diff_avg * 1, 2)} m³/s")
+print(f"- Average relative change = {np.round(diff_avg_rel * 100, 2)}%")
+print(f"- Maximum absolute change = {np.round(diff_max * 1, 2)} m³/s")
+print(f"- Maximum relative change = {np.round(diff_max_rel * 100, 2)}%")
+print(f"- Minimum absolute change = {np.round(diff_min * 1, 2)} m³/s")
+print(f"- Minimum relative change = {np.round(diff_min_rel * 100, 3)}%")
